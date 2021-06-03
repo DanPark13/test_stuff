7ba690c6c62ffc543b13fe6e7b0503d834373847
Daniel Park <dpark0703@gmail.com>
2021-06-02 20:31:08 -0500

    Add Two Simple Test Cases
    - Sum
    - Difference

---
+++ equations.py
@@ -0,0 +1,11 @@
+# Add one
+def add_one(x):
+    return x + 1
+
+# Find sum
+def sum(a,b):
+    return a+b
+
+# Find difference
+def difference(a,b):
+    return a-b
---
+++ test_equations.py
@@ -0,0 +1,10 @@
+from equations import *
+
+def test_always_passes():
+    assert True
+
+def test_sum():
+    assert sum(1,1) == 2
+
+def test_diff():
+    assert difference(2,1) == 1
